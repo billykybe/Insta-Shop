c38efbcb23d681fb75b3ce6244cdbf9409fec600
billykybe <billykibet2@gmail.com>
2022-03-03 03:48:15 +0300

    Bug 1 Fix

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,5 +1,22 @@
 const btnCloseTop = document.querySelector(".close");
 const topInfo = document.querySelector(".topInfo");
+var pop = document.getElementById("pop");
+var isPopped = false;
+
 btnCloseTop.addEventListener("click", function () {
   topInfo.classList.toggle("hide");
 });
+
+show();
+
+function show() {
+  setTimeout(function () {
+    pop.style.left = 0;
+    setTimeout(function () {
+      pop.style.left = "-100%";
+    }, 5000);
+    setTimeout(function () {
+      show();
+    }, 5000);
+  }, 2000);
+}
